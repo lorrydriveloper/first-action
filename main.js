675adf7db40c912446378730ed27bcf7a4c7014e
lorrydriveloper <lorrydriveloper@gmail.com>
2023-01-15 21:52:29 +0000

    remove

--- main.js
+++ main.js
@@ -19,8 +19,6 @@ async function main() {
       per_page: 100,
     })
 
-    core.info(JSON.stringify(result[0], null, 2))
-    core.info(JSON.stringify(core.summary, null, 2))
     core.setOutput('mergeableState', result[0].mergeable_state)
     core.info(result[0].mergeable_state)
   } catch (error) {
