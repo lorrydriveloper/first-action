f52c0c9177b3536e8e8a2fc151646aab0d4c2cc7
lorrydriveloper <lorrydriveloper@gmail.com>
2023-04-03 20:01:17 +0100

    other test

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const { paginateRest } = require('@octokit/plugin-paginate-rest')
 
 async function main() {
   try {
-    console.log('hello')
     const owner = core.getInput('owner')
     const repo = core.getInput('repo')
     const token = core.getInput('github-token', { required: true })
@@ -12,17 +11,43 @@ async function main() {
 
     const MyOctokit = Octokit.plugin(paginateRest)
     const octokit = new MyOctokit({ auth: 'token ' + token })
-    core.info(`Checking Status of ${owner}/${repo}/${pull_number}`)
-    const result = await octokit.paginate('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
-      owner: owner,
-      repo: repo,
-      pull_number: pull_number,
-      per_page: 100,
+
+    const required_users = ['lorrydriveloper', 'simonpjones', 'madwire']
+    const reviews = await octokit.pulls.listReviews({
+      owner,
+      repo,
+      pull_number,
+    })
+    // core.info(JSON.stringify(reviews.data, null, 2))
+    let approved_users = []
+    let change_request_users = []
+    // Get the usernames of users who have approved the pull request
+    reviews.data.forEach((review) => {
+      let user = review.user.login
+      if (review.state === 'APPROVED') {
+        approved_users.push(user)
+        approved_users = [...new Set(approved_users)]
+        change_request_users = change_request_users.filter((item) => item != user)
+      } else if (review.state === 'CHANGES_REQUESTED') {
+        change_request_users.push(user)
+        change_request_users = [...new Set(change_request_users)]
+      }
     })
 
-    core.setOutput('mergeableState', result[0].mergeable_state)
-    core.info(result[0].mergeable_state)
-    core.info(JSON.stringify(result, null, 2))
+    console.log(`Approved users: ${approved_users.join(', ')}`)
+    console.log(`change Request Users: ${change_request_users.join(', ')}`)
+
+    // Check if there are no changes requested and at least one of the required users has approved the pull request
+    const has_required_approval = approved_users.some((user) => required_users.includes(user))
+    const has_changes_requested = change_request_users.length > 0
+
+    if (!has_changes_requested && has_required_approval) {
+      core.setOutput('reviewState', 'ready')
+      console.log('The pull request has no changes requested and has been approved by at least one required user.')
+    } else {
+      core.setOutput('reviewState', 'ready')
+      console.log('The pull request has changes requested or has not been approved by any required user.')
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
